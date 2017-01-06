c06fcc8d8a8ef84a5a4caa5e31c09c347e8b7001
Andre <Andre@DESKTOP-EVCUACT>
2017-01-06 05:40:59 -0200

    First commit

---
+++ core.system.navbar.js
@@ -0,0 +1,100 @@
+/**
+ * Core Navbar
+ * @author André Ferreira <andrehrf@gmail.com>
+ */
+
+let SystemException = require("../core.plugins.exception.js");
+
+module.exports = {
+    /**
+     * List of navbar itens
+     * @type object
+     */
+    itens: {"File": {}, "Edit": {}},
+    
+    /**
+     * List module assets
+     * @type object
+     */
+    assets: {
+        css: [__dirname + "/style.css"],
+        js: [__dirname + "/events.js"]
+    },
+    
+    /**
+     * Function to add item menu
+     * 
+     * @params object item
+     * @return this
+     */ 
+    addItem: function(path, item, index){
+        if(typeof item == "object" && typeof path == "string")
+            this.itens[path] = item;
+        else
+            throw new SystemException("The default value for navbar item is 'object' and path is 'string'.");
+    },
+    
+    /**
+     * Funcion to create navbar
+     * 
+     * @param object webide
+     * @return object
+     */
+    createNavbar: function(webide){
+        let navbar = {};
+        
+        for(let key in this.itens){
+            let mapTree = key.split("/");
+            
+            for(let keyMapTree in mapTree){                
+                switch(parseInt(keyMapTree)){
+                    case 0:
+                        if(typeof navbar[mapTree[0]] !== "object")
+                            navbar[mapTree[0]] = {display: mapTree[keyMapTree], itens: {}};
+                        
+                        if(mapTree.length == 1){
+                            let command = webide.commands.get(this.itens[key].command);
+                            
+                            if(command)
+                                navbar[mapTree[0]].shortcut = command.bind.win;
+                            
+                            navbar[mapTree[0]].icon = this.itens[key].icon;
+                        }
+                    break;
+                    case 1:
+                        if(typeof navbar[mapTree[0]].itens[mapTree[1]] !== "object")
+                            navbar[mapTree[0]].itens[mapTree[1]] = {display: mapTree[keyMapTree], itens: {}};
+                        
+                        if(mapTree.length == 2){
+                            let command = webide.commands.get(this.itens[key].command);
+                            
+                            if(command)
+                                if(command.bind)
+                                    navbar[mapTree[0]].itens[mapTree[1]].shortcut = command.bind.win;
+                            
+                            navbar[mapTree[0]].itens[mapTree[1]].icon = this.itens[key].icon;
+                            navbar[mapTree[0]].itens[mapTree[1]].divide = this.itens[key].divide;
+                            navbar[mapTree[0]].itens[mapTree[1]].submenu = this.itens[key].submenu;
+                            navbar[mapTree[0]].itens[mapTree[1]].checkbox = this.itens[key].checkbox;
+                            navbar[mapTree[0]].itens[mapTree[1]].class = this.itens[key].class;
+                            navbar[mapTree[0]].itens[mapTree[1]].onclick = this.itens[key].onclick;
+                        }
+                    break;
+                }
+            }
+        }
+
+        return navbar;
+    },
+    
+    /**
+     * Function to generate template
+     * 
+     * @param object webide
+     * @return string
+     */
+    getTemplate: function(webide){
+        let fs = require("fs"), ejs = require("ejs"); 
+        return ejs.render(fs.readFileSync(__dirname + "/tpl.ejs").toString(), {itens: this.createNavbar(webide)});
+    }
+}
---
+++ events.js
@@ -0,0 +1,40 @@
+(function(){
+    webide.navbar = {clicked: false};    
+    var itens = document.querySelectorAll(".wi-navbar-item");
+    
+    document.querySelector(".wi-window-modal").onclick = function(){
+        webide.navbar.clicked = false;
+        document.querySelector(".wi-navbar").className = document.querySelector(".wi-navbar").className.replace("wi-navbar-clicked", "");
+    };
+    
+    for(var key in itens){
+        itens[key].onclick = function(e){
+            e.preventDefault();
+            e.stopPropagation();
+                
+            webide.navbar.clicked = !webide.navbar.clicked;
+            
+            if(webide.navbar.clicked){
+                document.querySelector(".wi-navbar").className = document.querySelector(".wi-navbar").className + " wi-navbar-clicked";
+                this.className = this.className + " wi-navbar-item-selected";
+            }
+            else{
+                document.querySelector(".wi-navbar").className = document.querySelector(".wi-navbar").className.replace("wi-navbar-clicked", "");
+                this.className = this.className.replace(" wi-navbar-item-selected", "");
+            }
+        }
+        
+        itens[key].onmouseover = function(){   
+            if(webide.navbar.clicked){
+                var _this = this;
+                var itens = document.querySelectorAll(".wi-navbar-item");
+                
+                for(var key2 in itens)
+                    if(typeof itens[key2].className == "string")
+                        itens[key2].className = itens[key2].className.replace(" wi-navbar-item-selected", "");
+
+                this.className = this.className + " wi-navbar-item-selected";      
+            }
+        }        
+    }
+})();
